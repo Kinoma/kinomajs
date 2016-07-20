943a86a13a457b962ae56e05b452dd9ff3668609
mkellner <mkellner@kinoma.com>
2016-07-19 22:11:33 -0700

    linux/gtk

--- xs6/xsedit/code/kprCodeLinux.c
+++ xs6/xsedit/code/kprCodeLinux.c
@@ -14,6 +14,9 @@
  *     See the License for the specific language governing permissions and
  *     limitations under the License.
  */
+
+#define __FSKWINDOW_PRIV__
+
 #include <sys/socket.h>
 #include <sys/ioctl.h>
 #include <linux/if.h>
